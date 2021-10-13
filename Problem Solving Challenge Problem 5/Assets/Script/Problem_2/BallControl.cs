4aa3a0af7ad986870a500710e8f343e4287c17a9
YosuaCM <90274542+YosuaCM@users.noreply.github.com>
2021-10-13 20:31:59 +0700

    Problem Solving Challenge Problem 5

---
+++ Problem Solving Challenge Problem 5/Assets/Script/Problem_2/BallControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControl : MonoBehaviour
+{
+    // Rigidbody 2D for ball
+    private Rigidbody2D rigidbody2d;
+
+    // Bola bergerak secara random dan kecepatan yang konstan
+    void PushBall()
+    {
+        float a = Random.Range(-100, 100);
+        float b = Random.Range(-100, 100);
+
+        rigidbody2d.AddForce(new Vector2(a, b).normalized * 50);  
+    }
+
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        PushBall();
+    }
+
+
+
+
+
+
+
+
+}
---
+++ Problem Solving Challenge Problem 5/Assets/Script/Problem_5/FollowCursor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCursor : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    
+    // Bola mengikuti kursor
+    void FixedUpdate()
+    {
+        Vector2 follow = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = Vector2.MoveTowards(transform.position, follow, 10 * Time.deltaTime);
+    }
+
+}
