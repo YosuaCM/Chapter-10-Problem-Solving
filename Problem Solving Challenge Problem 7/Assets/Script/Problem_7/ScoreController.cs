d15f30a8156fe4e8413d93494e4da23f6f9bdfa9
YosuaCM <90274542+YosuaCM@users.noreply.github.com>
2021-10-13 20:58:28 +0700

    Problem Solving Challenge Problem 7

---
+++ Problem Solving Challenge Problem 7/Assets/Script/Problem_7/ScoreController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreController : MonoBehaviour
+{
+    public Text scoree;
+    private int score = 0 ;
+
+    
+    void Start()
+    {
+        scoree.text = "Score : " + score;    
+    }
+
+    private void OnTriggerEnter2D(Collider2D box)
+    {
+        if (box.tag == "touchbox")
+        {
+            score++;
+            Destroy(box.gameObject);
+            scoree.text = "Score : " + score;
+        }
+    }
+    
+}
